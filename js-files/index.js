84cf2b94a45b8a80511b82c7e224b54e30551423
Chenhao Wang <implexrr@gmail.com>
2022-10-11 17:08:40 -0400

    Add functionality to grid size slider

--- js-files/index.js
+++ js-files/index.js
@@ -12,15 +12,55 @@
 
 // Real code below
 
+// Code for grid size
+// Add event listener to grid size slider
+let grid_size_slider = document.querySelector('#grid_size');
+grid_size_slider.addEventListener('change', create_grid);
 
+// Select canvas, create box template
 let canvas = document.querySelector('#canvas');
 let box = document.createElement('div');
-let width = 0;
-box.classList.add('box');
 
-for (let i = 0; i < (16 * 16); i ++) {
-  box = document.createElement('div');
-  box.classList.add('box');
-  console.log(canvas.style.width);
-  canvas.appendChild(box);
+// Create initial grid
+let grid_size = 16;
+let box_width = 500/16;
+let box_height = 500/16;
+create_grid();
+
+
+// Creates grid based on value of grid size
+function create_grid (e) {
+  get_grid_size();
+  remove_grid();
+  draw_grid();
+  update_size_text();
+}
+
+// Get grid size
+function get_grid_size () {
+  grid_size = grid_size_slider.value;
+  box_width = 500/grid_size;
+  box_height = 500/grid_size;
+}
+
+// Remove current grid from canvas
+function remove_grid () {
+  canvas.textContent = '';
+}
+
+// Draw grid on canvas
+function draw_grid () {
+  for (let i = 0; i < (grid_size * grid_size); i ++) {
+    box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = box_height + 'px';
+    box.style.height = box_height + 'px';
+    canvas.appendChild(box);
+    }
+}
+
+// Update the grid size display below the canvas
+function update_size_text (e) {
+  document.querySelector('#size_text').textContent 
+  = `Grid size: ${grid_size} x ${grid_size}`;
 }
