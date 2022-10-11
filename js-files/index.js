20b5eae3b8043f1aae87acbf042074353d233a2f
Chenhao Wang <implexrr@gmail.com>
2022-10-10 20:10:10 -0400

    Begin work on grid, sliders, toggles

--- js-files/index.js
+++ js-files/index.js
@@ -1,11 +1,26 @@
-const check_boxes = document.querySelectorAll(".check");
-for (let i = 0; i < check_boxes.length; i++) {
-  check_boxes[i].addEventListener('click', testCheck)
-}
-function testCheck(e) {
-  if (e.target.checked == true){
-    document.querySelector('body').style.backgroundColor = 'red';
-  } else {
-    document.querySelector('body').style.backgroundColor = 'white';
-  }
+// const check_boxes = document.querySelectorAll(".check");
+// for (let i = 0; i < check_boxes.length; i++) {
+//   check_boxes[i].addEventListener('click', testCheck)
+// }
+// function testCheck(e) {
+//   if (e.target.checked == true){
+//     document.querySelector('body').style.backgroundColor = 'red';
+//   } else {
+//     document.querySelector('body').style.backgroundColor = 'white';
+//   }
+// }
+
+// Real code below
+
+
+let canvas = document.querySelector('#canvas');
+let box = document.createElement('div');
+let width = 0;
+box.classList.add('box');
+
+for (let i = 0; i < (16 * 16); i ++) {
+  box = document.createElement('div');
+  box.classList.add('box');
+  console.log(canvas.style.width);
+  canvas.appendChild(box);
 }
